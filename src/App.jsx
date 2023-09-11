30440228342fb3695b9ac01c8ee69f233e1a2e2f
shivansh13579 <shivansh0975@gmail.com>
2023-09-12 05:18:08 +0530

    project done

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,13 @@ function App() {
     <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
     <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
       <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-        test
+      <button onClick={()=>setColor("Red")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'Red'}}>Red</button>
+      <button onClick={()=>setColor("Green")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'Green'}}>Green</button>
+      <button onClick={()=>setColor("Blue")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'Blue'}}>Blue</button>
+      <button onClick={()=>setColor("Yellow")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'Yellow'}}>Yellow</button>
+      <button onClick={()=>setColor("Pink")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'Pink'}}>Pink</button>
+      <button onClick={()=>setColor("Black")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:'Black'}}>Black</button>
+      
       </div>
      </div>
     </div>
